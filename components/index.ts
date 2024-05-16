5ccca3a02065c65ca70cf624d20c53d41f5bb267
ViraMesha <wira.mescha@gmail.com>
2024-05-17 00:15:00 +0300

    feat: Add DoughnutChart component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { HeaderBox } from "./HeaderBox";
 export { TotalBalanceBox } from "./TotalBalanceBox";
 export { AnimatedCounter } from "./AnimatedCounter";
+export { DoughnutChart } from "./DoughnutChart";
