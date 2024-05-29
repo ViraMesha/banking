24e0225254811c100191ce83b6eca3c632355d46
ViraMesha <wira.mescha@gmail.com>
2024-05-29 22:27:57 +0300

    feat: Add footer to AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -96,6 +96,20 @@ export const AuthForm = ({ type }: Props) => {
               </Button>
             </form>
           </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
+            </Link>
+          </footer>
         </>
       )}
     </section>
