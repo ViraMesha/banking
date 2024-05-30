b5a308dde54124bb3a7422d7f994c433d3c71466
ViraMesha <wira.mescha@gmail.com>
2024-05-30 23:30:33 +0300

    feat: add comments and two getters to createAdminClient function

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,13 +1,20 @@
 "use server";
-import { Client, Account } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
+/**
+ * Creates a new session client with the specified endpoint and project.
+ * Retrieves the session from cookies and sets it in the client.
+ * Throws an error if no session is found.
+ * @returns An object with an 'account' property that allows access to the account related functionalities.
+ */
 export async function createSessionClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  const session = cookies().get("my-custom-session");
+  const session = cookies().get("appwrite-session");
+
   if (!session || !session.value) {
     throw new Error("No session");
   }
@@ -15,12 +22,21 @@ export async function createSessionClient() {
   client.setSession(session.value);
 
   return {
+    /**
+     * Getter method to retrieve an instance of the Account class.
+     * @returns {Account} An instance of the Account class.
+     */
     get account() {
       return new Account(client);
     },
   };
 }
 
+/**
+ * Asynchronously creates an admin client for interacting with the Appwrite API.
+ * The client is configured with the endpoint, project ID, and API key from environment variables.
+ * @returns An object containing getters for accessing account, database, and user resources.
+ */
 export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -28,8 +44,26 @@ export async function createAdminClient() {
     .setKey(process.env.NEXT_APPWRITE_KEY!);
 
   return {
+    /**
+     * Getter for accessing the account resource using the provided client.
+     * @returns An Account instance for interacting with account-related operations.
+     */
     get account() {
       return new Account(client);
+    },
+    /**
+     * Getter for accessing the database resource using the provided client.
+     * @returns A Databases instance for interacting with database-related operations.
+     */
+    get database() {
+      return new Databases(client);
+    },
+    /**
+     * Getter for accessing the user resource using the provided client.
+     * @returns A Users instance for interacting with user-related operations.
+     */
+    get user() {
+      return new Users(client);
     },
   };
 }
