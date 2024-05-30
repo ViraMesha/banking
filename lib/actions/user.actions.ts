b2be633be5618dfa638078c0b509250cf6f67f29
ViraMesha <wira.mescha@gmail.com>
2024-05-30 23:33:56 +0300

    feat: Build a utility function to get the logged in user from Appwrite

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { createSessionClient } from "../appwrite";
+
 export const signIn = async () => {
   try {
     // Mutation / Modify database / Make fetch
@@ -15,3 +17,17 @@ export const signUp = async (userData: SignUpParams) => {
     console.error("Error", error);
   }
 };
+
+/**
+ * Asynchronously retrieves the logged-in user's account information.
+ * This function first creates a session client and then fetches the account details.
+ * @returns {Promise<Object|null>} A promise that resolves with the account information if successful, or null if an error occurs.
+ */
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
