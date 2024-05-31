fe97ebf2b6e854f2d51b8fb8b549d45b9e4307b1
ViraMesha <wira.mescha@gmail.com>
2024-05-31 23:56:12 +0300

    feat: Implement signUp function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
 export const signIn = async () => {
   try {
@@ -10,9 +13,34 @@ export const signIn = async () => {
   }
 };
 
+/**
+ * Creates a new user account with the provided user data.
+ * @param userData - An object containing user data including email, password, first name, and last name.
+ * @returns A parsed stringified version of the newly created user account.
+ */
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+  const name = `${firstName} ${lastName}`;
   try {
     // Create a user account
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      name
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error", error);
   }
