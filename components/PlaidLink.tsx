da02de22501cfdc2c19cb59ac6b5be27b54bd673
ViraMesha <wira.mescha@gmail.com>
2024-06-12 13:21:50 +0300

    feat: Add functionality to PlaidLink

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,10 +1,46 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { PlaidLinkOptions, usePlaidLink } from "react-plaid-link";
 import { Button } from "./ui/button";
 
 export const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const getLinkToken = async () => {
+      // const data = await createLinkToken(user);
+      // setToken(data?.linkToken)
+    };
+
+    getLinkToken();
+  }, []);
+
+  const onSuccess = useCallback(
+    async (public_token: string) => {
+      // await exchangePublicToken({publicToken:public_token, user})
+      router.push("/");
+    },
+    [user?.$id]
+  );
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
+
+  const { open, ready } = usePlaidLink(config);
+
   return (
     <>
       {variant === "primary" ? (
-        <Button className="plaidlink-primary">Connect bank</Button>
+        <Button
+          onClick={() => open()}
+          disabled={!ready}
+          className="plaidlink-primary"
+        >
+          Connect bank
+        </Button>
       ) : variant === "ghost" ? (
         <Button>Connect bank</Button>
       ) : (
