64dcfdcdd7fb3796346fe62315518ace35dce75b
ViraMesha <wira.mescha@gmail.com>
2024-06-22 12:52:29 +0300

    feat: Create getAccounts func

---
+++ lib/actions/bank.actions.ts
@@ -0,0 +1,68 @@
+"use server";
+
+import {
+  ACHClass,
+  CountryCode,
+  TransferAuthorizationCreateRequest,
+  TransferCreateRequest,
+  TransferNetwork,
+  TransferType,
+} from "plaid";
+
+import { parseStringify } from "../utils";
+
+// import { getTransactionsByBankId } from "./transaction.actions";
+import { getBanks, getBank } from "./user.actions";
+import { plaidClient } from "../plaid";
+
+// Get multiple bank accounts belonging to a specific user
+export const getAccounts = async ({ userId }: getAccountsProps) => {
+  try {
+    // get all banks from db
+    const banks = await getBanks({ userId });
+
+    // get access to all the accounts within those banks
+    const accounts: Account[] = await Promise.all(
+      banks?.map(async (bank: Bank): Promise<Account> => {
+        // get each account info from plaid
+        const accountsResponse = await plaidClient.accountsGet({
+          access_token: bank.accessToken,
+        });
+
+        const accountData = accountsResponse.data.accounts[0];
+
+        // get institution info from plaid
+        const institution = await getInstitution({
+          institutionId: accountsResponse.data.item.institution_id!,
+        });
+
+        const account: Account = {
+          id: accountData.account_id,
+          availableBalance: accountData.balances.available!,
+          currentBalance: accountData.balances.current!,
+          institutionId: institution.institution_id,
+          name: accountData.name,
+          officialName: accountData.official_name!,
+          mask: accountData.mask!,
+          type: accountData.type as string,
+          subtype: accountData.subtype! as string,
+          appwriteItemId: bank.$id,
+          sharableId: bank.sharableId,
+        };
+
+        return account;
+      })
+    );
+
+    const totalBanks = accounts.length;
+
+    const totalCurrentBalance = accounts.reduce(
+      (total, account) => total + account.currentBalance,
+      0
+    );
+
+    return parseStringify({ data: accounts, totalBanks, totalCurrentBalance });
+  } catch (error) {
+    console.error("An error occurred while getting the accounts:", error);
+  }
+};
