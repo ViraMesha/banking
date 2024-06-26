173bc61b0071bfd8b8a85d156d8cd0a1ee29ba6f
ViraMesha <wira.mescha@gmail.com>
2024-06-26 12:42:57 +0300

    refactor: Desctructure obj properties

--- components/BankInfo.tsx
+++ components/BankInfo.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/lib/utils";
 
 export const BankInfo = ({ account, appwriteItemId, type }: BankInfoProps) => {
+  const { subtype, name, currentBalance } = account;
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -26,45 +27,46 @@ export const BankInfo = ({ account, appwriteItemId, type }: BankInfoProps) => {
   };
 
   const colors = getAccountTypeColors(account?.type as AccountTypes);
+  const { bg, lightBg, title, subText } = colors;
 
   return (
     <button
       onClick={handleBankChange}
-      className={cn(`bank-info ${colors.bg}`, {
+      className={cn(`bank-info ${bg}`, {
         "shadow-sm border-blue-700": type === "card" && isActive,
         "rounded-xl": type === "card",
         "hover:shadow-sm cursor-pointer": type === "card",
       })}
     >
       <figure
-        className={`flex-center h-fit rounded-full bg-blue-100 ${colors.lightBg}`}
+        className={`flex-center h-fit rounded-full bg-blue-100 ${lightBg}`}
       >
         <Image
           src="/icons/connect-bank.svg"
           width={20}
           height={20}
-          alt={account.subtype}
+          alt={subtype}
           className="m-2 min-w-5"
         />
       </figure>
       <div className="flex w-full flex-1 flex-col justify-center gap-1">
         <div className="bank-info_content">
           <h2
-            className={`text-16 line-clamp-1 flex-1 font-bold text-blue-900 ${colors.title}`}
+            className={`text-16 line-clamp-1 flex-1 font-bold text-blue-900 ${title}`}
           >
-            {account.name}
+            {name}
           </h2>
           {type === "full" && (
             <p
-              className={`text-12 rounded-full px-3 py-1 font-medium text-blue-700 ${colors.subText} ${colors.lightBg}`}
+              className={`text-12 rounded-full px-3 py-1 font-medium text-blue-700 ${subText} ${lightBg}`}
             >
-              {account.subtype}
+              {subtype}
             </p>
           )}
         </div>
 
-        <p className={`text-16 font-medium text-blue-700 ${colors.subText}`}>
-          {formatAmount(account.currentBalance)}
+        <p className={`text-16 font-medium text-blue-700 ${subText}`}>
+          {formatAmount(currentBalance)}
         </p>
       </div>
     </button>
