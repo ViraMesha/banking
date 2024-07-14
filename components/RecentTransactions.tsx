e59c68e9349e89fcc77829262895b8a63316aba4
ViraMesha <wira.mescha@gmail.com>
2024-07-14 14:46:22 +0300

    feat: Replace transactions with currentTransactions in TransactionsTable

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -64,7 +64,7 @@ export const RecentTransactions = ({
                   appwriteItemId={appwriteItemId}
                   type="full"
                 />
-                <TransactionsTable transactions={transactions} />
+                <TransactionsTable transactions={currentTransactions} />
                 <Pagination totalPages={totalPages} page={page} />
               </TabsContent>
             </Fragment>
